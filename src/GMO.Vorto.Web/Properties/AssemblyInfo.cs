dd6a1145fcbf714b3ac471af884b13f54fbe0c36
Gunnar Már Óttarsson <gunni@starcraft.is>
2018-01-09 12:26:55 +0200

    Split up project

--- src/GMO.Vorto.Web/Properties/AssemblyInfo.cs
+++ src/GMO.Vorto.Web/Properties/AssemblyInfo.cs
@@ -2,36 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GMO.Vorto")]
+[assembly: AssemblyTitle("GMO.Vorto.Web")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GMO.Vorto")]
-[assembly: AssemblyCopyright("Copyright ?  2014")]
+[assembly: AssemblyProduct("GMO.Vorto.Web")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c998f18b-4a4d-4c10-b35c-472c5e98c0b7")]
-[assembly: InternalsVisibleTo("GMO.Vorto.Web")]
+[assembly: Guid("c8ff754b-7fda-41c1-bbe3-0f3b0744e1b5")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.4")]
-[assembly: AssemblyVersion("1.4")]
-[assembly: AssemblyFileVersion("1.4")]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
